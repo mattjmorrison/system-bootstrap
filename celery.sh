44ac65d0ef326bf595df3c952693a371a18fd30d
Matthew J Morrison <mattj.morrison@gmail.com>
2010-12-12 12:57:57 -0600

    added startup of django and celery

--- celery.sh
+++ celery.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Setup RabbitMQ
 apt-get install -y rabbitmq-server
 rabbitmqctl add_user myuser mypassword
 rabbitmqctl add_vhost myvhost
 rabbitmqctl set_permissions -p myvhost myuser ".*" ".*" ".*"
 
+# install Git
 apt-get install -y git-core
+
+# checkout the Django-Celery-Buildout app
 git clone https://github.com/mattjmorrison/django-celery-buildout.git
 
+# Setup the Django-Celery-Buildout app
 cd django-celery-buildout
 python bootstrap.py
 bin/buildout
+bin/manage.py syncdb
+
+bin/manage.py runserver 0.0.0.0:80
+bin/manage.py celeryd
