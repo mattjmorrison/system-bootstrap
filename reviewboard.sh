b2bb238590502d02d4dbe0982fc7995a74f6bc81
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-03-22 18:19:20 -0500

    updated reviewboard script

--- reviewboard.sh
+++ reviewboard.sh
@@ -1,14 +1,14 @@
+
 apt-get install -y git-core
 apt-get install -y mysql-server
 apt-get install -y python-virtualenv
-mysqladmin -p create reviewboard
 apt-get install -y python-mysqldb
 apt-get install -y python-dev
 apt-get install -y memcached
 apt-get install -y patch
 
-pip install python-memcached
-pip install ReviewBoard
+
+# Create python virtualenv
 pip install virtualenvwrapper
 echo "export WORKON_HOME=~/Envs" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
@@ -18,4 +18,13 @@ if [ ! -d $WORKON_HOME ]; then
 fi
 mkvirtualenv default --no-site-packages
 workon default
+
+#install python packages
+pip install python-memcached
+pip install ReviewBoard
+
+
+
+mysqladmin -p create reviewboard
+
 
