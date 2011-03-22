12a73003a3734060193c32c138522a5785ae2b58
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-03-22 18:10:57 -0500

    added reviewboard bootstrap script

---
+++ reviewboard.sh
@@ -0,0 +1,20 @@
+apt-get install -y git-core
+apt-get install -y mysql-server
+apt-get install -y python-virtualenv
+mysqladmin -p create reviewboard
+apt-get install -y python-mysqldb
+apt-get install -y python-dev
+apt-get install -y memcached
+apt-get install -y patch
+
+pip install python-memcached
+pip install ReviewBoard
+pip install virtualenvwrapper
+echo "export WORKON_HOME=~/Envs" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
+if [ ! -d $WORKON_HOME ]; then
+        mkdir -p $WORKON_HOME
+fi
+mkvirtualenv default --no-site-packages
+
